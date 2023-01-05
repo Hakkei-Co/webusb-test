0676e4cbb365bbb747196fade24c93670b19d94a
Jeff Rizzo <riz@tastylime.net>
2023-01-05 09:25:47 -0800

    connect/reset are active now

--- connect.js
+++ connect.js
@@ -1,14 +1,21 @@
 const connectButton = document.getElementById("connect");
 const disconnectButton = document.getElementById("disconnect");
+const resetButton = document.getElementById("reset");
 
 let device;
 connectButton.onclick = async () => {
   device = await navigator.usb.requestDevice({
-    filters: [{ vendorId: 0x1d50 }]
+    filters: []
   });
   await device.open();
-  connected.style.display = 'block';
-  connectButton.style.display = 'none';
-  disconnectButton.style.display = 'initial';
+  console.log(device);
+  //connected.style.display = 'block';
+  //connectButton.style.display = 'none';
+  //disconnectButton.style.display = 'initial';
 };
 
+resetButton.onclick = async () => {
+    console.log("Device before reset: ", device);
+    await device.reset();
+    console.log("Device after reset:", device);
+}
